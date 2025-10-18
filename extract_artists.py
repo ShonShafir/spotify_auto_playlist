829e15d4cf1ad49cf2133a3ea58376a18d33faf5
DJ Universe <104515745+ShonShafir@users.noreply.github.com>
2025-10-18 11:52:47 +0300

    Refactor Spotify client initialization and artist extraction

--- extract_artists.py
+++ extract_artists.py
@@ -1,20 +1,6 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 import config
-import os
-os.environ['SPOTIPY_CACHE'] = '.spotify_cache'
-
-def get_spotify_client():
-    scope = "playlist-modify-public playlist-modify-private"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=config.SPOTIFY_CLIENT_ID,
-        client_secret=config.SPOTIFY_CLIENT_SECRET,
-        redirect_uri=config.SPOTIFY_REDIRECT_URI,
-        scope=scope,
-        cache_path=".spotify_cache",
-        open_browser=False
-    ))
-    return sp
 
 def extract_playlist_id(url):
     if 'playlist/' in url:
@@ -25,26 +11,30 @@ def get_all_tracks(sp, playlist_id):
     tracks = []
     results = sp.playlist_tracks(playlist_id)
     tracks.extend(results['items'])
+
     while results['next']:
         results = sp.next(results)
         tracks.extend(results['items'])
     return tracks
 
 def extract_artist_ids():
-    sp = get_spotify_client()
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
+        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET")
+    )
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
     artist_ids = set()
-    for url in config.PLAYLIST_URLS:
-        playlist_id = extract_playlist_id(url)
+    for playlist_url in config.PLAYLIST_URLS:
+        playlist_id = extract_playlist_id(playlist_url)
         tracks = get_all_tracks(sp, playlist_id)
         for item in tracks:
             if item['track'] and item['track']['artists']:
                 for artist in item['track']['artists']:
                     artist_ids.add(artist['id'])
-    artist_ids = [aid for aid in artist_ids if aid]
+
     with open(config.ARTISTS_FILE, 'w') as f:
         f.write(', '.join(artist_ids))
-    print(f"Saved {len(artist_ids)} artist IDs to {config.ARTISTS_FILE}")
-    return len(artist_ids)
 
-if __name__ == '__main__':
-    extract_artist_ids()
+    print(f"Saved {len(artist_ids)} unique artist IDs to {config.ARTISTS_FILE}")
+    return len(artist_ids)
